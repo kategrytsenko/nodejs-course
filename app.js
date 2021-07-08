a3b76237b46176f9f00328c9392e73192f710da8
kategrytsenko <grytsenko.kate.ua@gmail.com>
2021-07-08 16:17:30 +0300

    NC: initial commit: added express

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// app.use('/add-product', (req, res, next) => {
+//     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+//     next();
+// });
+
+// app.post('/product', (req, res, next) => {
+//     console.log(req.body);
+//     res.redirect('/');
+// });
+
+// app.get('/', (req, res, next) => {
+//     res.send('<h1>Hello from Express!</h1>');
+// });
+
+// const routes = require('./routes');
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.listen(3000);
